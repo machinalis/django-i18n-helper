db160a1ae9b0905dba33f29d85cbd283fa06dc08
Santiago Romero <santi.romero@gmail.com>
2012-08-27 10:10:37 -0300

    Updated docs. Pypi package info and files.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+with open('README.md') as readme:
+    __doc__ = readme.read()
+
+from distutils.core import setup
+
+setup(
+    name='django-i18n-helper',
+    version='0.1',
+    description=u'A internationalization helper that highlights translated strings',
+    long_description=__doc__,
+    author = u'Santiago Gabriel Romero',
+    author_email = 'sromero@machinalis.com',
+    url='https://github.com/machinalis/django-i18n-helper',
+    packages=['i18n_helper'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
